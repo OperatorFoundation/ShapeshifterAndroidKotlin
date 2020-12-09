98c95aa678ca70a031f4a121c18ba913fd888d8c
Bluesaxorcist <joshua@operatorfoundation.org>
2020-12-09 15:20:51 -0600

    Made shadow compatible with OKHTTP

---
+++ Shadow/src/main/java/org/operatorfoundation/shapeshifter/shadow/kotlin/OKHTTPShadowSocket.kt
@@ -0,0 +1,12 @@
+package org.operatorfoundation.shapeshifter.shadow.kotlin
+
+import java.net.SocketAddress
+
+class OKHTTPShadowSocket(config: ShadowConfig) : ShadowSocket(config) {
+
+    @ExperimentalUnsignedTypes
+    override fun connect(endpoint: SocketAddress?) {
+
+    }
+
+}
