926b2d3271b7babf53724b06908d5efb3d7724e5
consuelita <adelita@operatorfoundation.org>
2022-04-25 17:18:19 -0500

    Update LaunchActivity.kt

--- shadowexampleapp/src/main/java/org/operatorfoundation/shadowexampleapp/LaunchActivity.kt
+++ shadowexampleapp/src/main/java/org/operatorfoundation/shadowexampleapp/LaunchActivity.kt
@@ -9,6 +9,7 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import org.operatorfoundation.shapeshifter.shadow.kotlin.ShadowConfig
 import org.operatorfoundation.shapeshifter.shadow.kotlin.ShadowSocket
+import org.operatorfoundation.shapeshifter.shadow.kotlin.toHexString
 import kotlin.coroutines.CoroutineContext
 import kotlin.coroutines.EmptyCoroutineContext
 
@@ -28,28 +29,33 @@ class LaunchActivity : AppCompatActivity()
         }
     }
     // TODO: Replace this with a good example of how to use our library. What does this library do and how do we use it...
-    suspend fun shadowDarkStarClient() {
-
+    suspend fun shadowDarkStarClient()
+    {
         externalScope.launch(defaultDispatcher)
         {
-
             // TODO: Make sure password matches the servers public key.
             val config = ShadowConfig(
                 "9caa4132c724f137c67928e9338c72cfe37e0dd28b298d14d5b5981effa038c9",
                 "DarkStar"
             )
+
             // TODO: Use a valid server IP address.
             val shadowSocket = ShadowSocket(config, "0.0.0.0", 1234)
             val httpRequest = "GET / HTTP/1.0\r\n\r\n"
             val textBytes = httpRequest.toByteArray()
+
             shadowSocket.outputStream.write(textBytes)
             println("Wrote some bytes.")
+
             shadowSocket.outputStream.flush()
             println("Flushed the output stream.")
+            
             var buffer = ByteArray(5)
             System.out.println("bytes available: " + shadowSocket.inputStream.available())
-            shadowSocket.inputStream.read(buffer)
-            println("Read some bytes, test complete.")
+            val numberOfBytesRead = shadowSocket.inputStream.read(buffer)
+            println("Read $numberOfBytesRead bytes.")
+            println("Read some data: " + buffer.toHexString())
+            println("Test complete")
         }
     }
 }
